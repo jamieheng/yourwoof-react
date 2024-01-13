85f12bf17421bda27d7a28a6b11799e87c04b7d1
SeryVathana <yooseryvathana@gmail.com>
2024-01-13 08:59:07 +0700

    a

--- App/routes/index.js
+++ App/routes/index.js
@@ -1,13 +1,12 @@
 const routes = (app) => {
-    app.use("/pet", require("./pet"));
-    app.use("/user", require("./user"));
-    app.use("/userRequest", require("./userrequest"));
-    app.use("/adoptionRequest", require("./adoptionrequest"));
-    app.use("/adoption", require("./adoption"));
-    app.use("/admin", require("./admin"));
-    app.use("/tracking", require("./tracking"));
-    app.use("/surrender", require("./surrender"));
-  
-  };
-  
-  module.exports = routes;
+  app.use('/pet', require('./pet'));
+  app.use('/user', require('./user'));
+  app.use('/userRequest', require('./userRequest'));
+  app.use('/adoptionRequest', require('./adoptionRequest'));
+  app.use('/adoption', require('./adoption'));
+  app.use('/admin', require('./admin'));
+  app.use('/tracking', require('./tracking'));
+  app.use('/surrender', require('./surrender'));
+};
+
+module.exports = routes;
